8c28e2bf514fb3910f9f0e94a0a6f480ac8916cb
Bryce Bresnan <Bryce.Bresnan@gmail.com>
2022-10-23 01:57:44 -0700

    validate rating between 1 and 5

--- app/models/review.rb
+++ app/models/review.rb
@@ -4,4 +4,5 @@ class Review < ApplicationRecord
   validates :content_body, presence: true
   validates :rating, presence: true
   validates :content_body, length: {in: 50..250 }
+  validates :rating, numericality: {greater_than_or_equal_to: 1, less_than_or_equal_to: 5}
 end
