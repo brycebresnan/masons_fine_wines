041a40444e4764c53ed9c7678dc2b26fb3782b3e
Bryce Bresnan <Bryce.Bresnan@gmail.com>
2022-10-23 02:16:00 -0700

    create callback to titleize product name

--- app/models/product.rb
+++ app/models/product.rb
@@ -4,4 +4,11 @@ class Product < ApplicationRecord
   validates :country_of_origin, presence: true
   validates :cost, presence: true
   validates :cost, numericality: { greater_than: 0.00, less_than: 99999.99  }
+
+  before_save(:titleize_product)
+
+  private
+    def titleize_product
+      self.name = self.name.titleize
+    end
 end
--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -8,4 +8,10 @@ describe Product do
   it {should validate_presence_of :cost }
   it {should validate_numericality_of(:cost).is_greater_than(0.00)}
   it {should validate_numericality_of(:cost).is_less_than(99999.99)}
+
+  it("titleizes the name of a product") do
+    product = Product.create({name: "mason's reserve", country_of_origin: "USA", cost: 100.00})
+    expect(product.name()).to(eq("Mason's Reserve"))
+  end
+
 end
