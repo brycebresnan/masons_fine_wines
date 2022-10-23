bfeff83766fe3c50d4856d092edde3fef0229326
Bryce Bresnan <Bryce.Bresnan@gmail.com>
2022-10-23 01:22:03 -0700

    validate cost is greater than 0

--- app/models/product.rb
+++ app/models/product.rb
@@ -3,4 +3,5 @@ class Product < ApplicationRecord
   validates :name, presence: true
   validates :country_of_origin, presence: true
   validates :cost, presence: true
+  validates :cost, numericality: { greater_than: 0.00 }
 end
