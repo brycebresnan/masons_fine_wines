b7a51b2ba36f1ab480747ba7aee227ca0f1a4ccd
Bryce Bresnan <Bryce.Bresnan@gmail.com>
2022-11-03 14:40:15 -0700

    create users table

---
+++ db/migrate/20221103213751_create_users.rb
@@ -0,0 +1,13 @@
+class CreateUsers < ActiveRecord::Migration[7.0]
+  def change
+    create_table :users do |t|
+      t.column(:email, :string)
+      t.column(:user_name, :string)
+      t.column(:password_hash, :string)
+      t.column(:password_salt, :string)
+      t.column(:admin, :boolean)
+
+      t.timestamps()
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_10_23_065007) do
+ActiveRecord::Schema[7.0].define(version: 2022_11_03_213751) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -29,6 +29,16 @@ ActiveRecord::Schema[7.0].define(version: 2022_10_23_065007) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "product_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "email"
+    t.string "user_name"
+    t.string "password_hash"
+    t.string "password_salt"
+    t.boolean "admin"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   add_foreign_key "reviews", "products"
